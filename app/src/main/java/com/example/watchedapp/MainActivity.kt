faa13ffeb47b9902abeaf1e7423ee90fb2ccd4e7
Martin Jensen <martinloesethjensen@gmail.com>
2023-03-03 18:31:30 +0100

    fix: hilt annotations and manifest application

--- app/src/main/java/com/example/watchedapp/MainActivity.kt
+++ app/src/main/java/com/example/watchedapp/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.watchedapp.presentation.HomeScreen
+import com.example.watchedapp.presentation.home.HomeScreen
 import com.example.watchedapp.presentation.ui.theme.WatchedAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/watchedapp/WatchedApplication.kt
+++ app/src/main/java/com/example/watchedapp/WatchedApplication.kt
@@ -3,4 +3,5 @@ package com.example.watchedapp
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class WatchedApplication : Application()
