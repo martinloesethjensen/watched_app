3ba64f65146034ba4ea6e3d5b0260bc7ac71c9cd
Martin Jensen <martinloesethjensen@gmail.com>
2026-05-17 18:28:27 +0200

    fix

--- app/src/main/kotlin/dev/martinloeseth/watchedapp/domain/usecases/DeleteWatchHistoryEntryUseCase.kt
+++ app/src/main/kotlin/dev/martinloeseth/watchedapp/domain/usecases/DeleteWatchHistoryEntryUseCase.kt
@@ -3,7 +3,7 @@ package dev.martinloeseth.watchedapp.domain.usecases
 import dev.martinloeseth.watchedapp.domain.repositories.WatchHistoryRepository
 import javax.inject.Inject
 
-class DeleteWatchHistoryEntry @Inject constructor(
+class DeleteWatchHistoryEntryUseCase @Inject constructor(
     private val repository: WatchHistoryRepository
 ) {
     suspend operator fun invoke(id: Int) = repository.deleteEntry(id)
